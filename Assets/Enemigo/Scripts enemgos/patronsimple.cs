c3400e19aad6186025a401b7f42ee96b0585c303
ManuelAlejandro20 <38479974+ManuelAlejandro20@users.noreply.github.com>
2018-04-17 23:34:04 -0300

    Proyecto Joe

---
+++ Assets/Enemigo/Scripts enemgos/patronsimple.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patronsimple : MonoBehaviour
+{
+
+    public Transform puntopartida;
+    public Transform puntofinal;
+    public Transform localizador;
+    public float velocidadenemigo;
+    public Transform jugador;
+    public float distancia;
+    public bool persiguiendo = false;
+
+    // Use this for initialization
+
+    void acercamiento()
+    {
+
+        distancia = Vector3.Distance(jugador.position, transform.position);
+        if (distancia <= 10)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            GetComponent<Animator>().SetTrigger("ataque");
+            persiguiendo = true;
+            localizador.transform.position = new Vector3(jugador.transform.position.x - 3.3f, puntofinal.transform.position.y, puntofinal.transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, localizador.transform.position, velocidadenemigo * Time.deltaTime);
+            if (transform.position == localizador.transform.position)
+            {
+                persiguiendo = true;
+            }
+
+
+        }
+        else if (persiguiendo)
+        {
+            if (transform.position.x > puntofinal.transform.position.x)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            GetComponent<Animator>().SetTrigger("caminata");
+            transform.position = Vector3.MoveTowards(transform.position, puntofinal.transform.position, velocidadenemigo * Time.deltaTime);
+            if (transform.position == puntofinal.transform.position)
+            {
+                persiguiendo = false;
+            }
+
+        }   
+
+    }
+
+    void Start()
+    {
+
+        if (GetComponent<SpriteRenderer>().flipX == false)
+        {
+            transform.position = puntopartida.transform.position;
+
+        }
+        else
+        {
+
+            transform.position = puntofinal.transform.position;
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!persiguiendo)
+        {
+            if (GetComponent<SpriteRenderer>().flipX == false)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, puntofinal.transform.position, velocidadenemigo * Time.deltaTime);
+                if (transform.position == puntofinal.transform.position)
+                {
+                    GetComponent<SpriteRenderer>().flipX = true;
+                }
+
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, puntopartida.transform.position, velocidadenemigo * Time.deltaTime);
+                if (transform.position == puntopartida.transform.position)
+                {
+                    GetComponent<SpriteRenderer>().flipX = false;
+                }
+
+            }
+        }
+
+
+        acercamiento();
+
+
+
+    }
+}
---
+++ Assets/Joe/Imagen tesoro/Imagen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Imagen : MonoBehaviour {
+
+    bool activa = true;
+    Canvas canvas;
+    public GameObject cofre;
+
+	void Start () {
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+	}
+	
+	void Update () {
+        if (cofre.GetComponent<Animator>().GetBool("abierto") && activa)
+        {
+            canvas.enabled = true;
+            Time.timeScale = 0;
+            if (Input.GetKeyDown("x"))
+            {
+                canvas.enabled = false;
+                Time.timeScale = 1;
+                activa = false;
+            }
+        }
+	}
+}
