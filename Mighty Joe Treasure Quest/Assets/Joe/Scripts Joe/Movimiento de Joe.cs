2ac4a8400f93a87b8bc426f46ba16813912d42d9
ManuelAlejandro20 <38479974+ManuelAlejandro20@users.noreply.github.com>
2018-04-20 10:38:06 -0300

    cambio de practica
    hola cabros

---
+++ Mighty Joe Treasure Quest/Assets/Joe/Scripts Joe/Movimiento de Joe.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientodeJoe : MonoBehaviour {
+
+    [SerializeField] bool ensuelo = true;
+
+
+    public Rigidbody2D rigid;
+
+
+    void awake() {
+
+        rigid = GetComponent<Rigidbody2D>();
+
+    }
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    void movimiento() {
+
+        if (Input.GetKeyDown("z") && ensuelo) {
+
+
+
+
+        }
+
+
+    }
+
+    void saltar() { }
+
+    void agacharse() { }
+}
