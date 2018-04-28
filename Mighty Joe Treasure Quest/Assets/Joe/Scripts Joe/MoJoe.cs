4f9ffcdb1d09e9c0be9215edf53304782ca17760
ManuelAlejandro20 <38479974+ManuelAlejandro20@users.noreply.github.com>
2018-04-28 04:03:34 -0300

    cambios

--- Mighty Joe Treasure Quest/Assets/Joe/Scripts Joe/MoJoe.cs
+++ Mighty Joe Treasure Quest/Assets/Joe/Scripts Joe/MoJoe.cs
@@ -2,164 +2,188 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
 public class MoJoe : MonoBehaviour {
 
-    public LayerMask sueloLayer;
-    public Transform suelocheck;
-    public float JumpPower;
-    public float velocidad = 4f;
-    public bool ensuelo = true;
-    float timer = 0.2f;
-    float timer2 = 0f;
-    public float checkradius = 0.05f;
-    public GameObject proyectilprefab;
-    public bool disparo = false;
+    [SerializeField] bool ensuelo = true;
+    [SerializeField] Transform chequearsuelo;
 
-    void bala() {
+    bool disparo = false;
+    Rigidbody2D rigid;
+    Animator anim;
+
+    public LayerMask capasuelo;
+    public GameObject proyectilprefab;
+
+    float podersalto = 18f;
+    float velocidad = 6f;
+    float radiocirculosuelo = 0.05f;
+    public float tiempodisparo = 0f;
+    float tiemposalto = 0f;
+
+
+    void Awake() {
+
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+
+    }
+  
+    void Start() {
+
+            
+    }  
+      
+	void Update () {
+
+        movimientos();
+
+    }
+
+    void movimientos() {
+
+        tiemposalto += 2f * Time.deltaTime;
+
+        if (ensuelo && Input.GetKeyDown("z"))
+        {
+            anim.SetBool("parado", false);
+            anim.SetBool("saltar", true);
+            rigid.velocity = new Vector2(rigid.velocity.x, 0f);
+            rigid.AddForce(new Vector2(0, podersalto), ForceMode2D.Impulse);
+            ensuelo = false;
+
+        }
+
+        if (tiemposalto >= 0.2f)
+        {
+            anim.SetBool("saltar", false);
+            anim.SetBool("caer", false);
+            tiemposalto = 0;
+        }
+
+
+        if (!ensuelo && !anim.GetBool("saltar"))
+        {
+            ensuelo = Physics2D.OverlapCircle(chequearsuelo.position, radiocirculosuelo, capasuelo);
+            anim.SetBool("caminando y salto", ensuelo);
+        }
+
+        ensuelo = Physics2D.OverlapCircle(chequearsuelo.position, radiocirculosuelo, capasuelo);
+        anim.SetBool("parado", ensuelo);
+
+        if (Input.GetKey(KeyCode.RightArrow) && !anim.GetBool("agacharse") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Patada"))
+        {
+            if (GetComponent<SpriteRenderer>().flipX == true)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            anim.SetBool("parado", false);
+            anim.SetBool("caminando", true);
+            rigid.velocity = new Vector2(velocidad, rigid.velocity.y);
+        }
+
+        else if (Input.GetKeyUp(KeyCode.RightArrow) && ensuelo && !anim.GetBool("patada"))
+        {
+            anim.SetBool("caminando", false);
+            anim.SetBool("parado", true);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) && !anim.GetBool("agacharse") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Patada"))
+        {
+            if (GetComponent<SpriteRenderer>().flipX == false)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+
+            anim.SetBool("parado", false);
+            anim.SetBool("caminando", true);
+            rigid.velocity = new Vector2(-velocidad, rigid.velocity.y);
+
+        }
+
+        else if (Input.GetKeyUp(KeyCode.LeftArrow) && ensuelo && !anim.GetBool("patada"))
+        {
+            anim.SetBool("caminando", false);
+            anim.SetBool("parado", true);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            anim.SetBool("agacharse", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            anim.SetBool("agacharse", false);
+        }
+
+        if (Input.GetKey("x") && !disparo && ensuelo)
+        {
+            tiempodisparo += Time.deltaTime;
+            if (tiempodisparo >= 0.25f)
+            {
+                if (GetComponent<SpriteRenderer>().flipX == false)
+                {
+                    rigid.velocity = new Vector2(8f, rigid.velocity.y);
+                }
+                else
+                {
+                    rigid.velocity = new Vector2(-8f, rigid.velocity.y);
+                }
+
+                anim.SetBool("parado", true);
+                anim.SetBool("caminando", false);
+                anim.SetTrigger("patada");
+                disparo = true;
+                tiempodisparo = 0f;
+
+            }
+
+        }
+
+        else if (Input.GetKeyUp("x"))
+        {
+            disparo = false;
+        }
+
+        if (!ensuelo)
+        {
+
+            anim.SetBool("caer", true);
+
+        }
+
+        if (Input.GetKeyDown("c") && anim.GetBool("parado"))
+        {
+
+            rigid.velocity = new Vector2(rigid.velocity.x, 15f);
+            anim.SetTrigger("upper");
+
+
+        }
+
+        else if (Input.GetKeyDown("c") && anim.GetBool("caminando") && ensuelo)
+        {
+
+            rigid.velocity = new Vector2(rigid.velocity.x, 15f);
+            anim.SetTrigger("upper");
+
+        }
+
+
+
+
+
+    }
+
+
+    void bala()
+    {
 
         Instantiate(proyectilprefab, transform.position, Quaternion.identity);
 
     }
 
 
-  
-    // Use this for initialization  
-    void Start() {
 
-            
-    }  
-      
-   
-	// Update is called once per frame
-	void Update () {
-
-        if (ensuelo && Input.GetAxis("Jump")>0)
-        {
-            GetComponent<Animator>().SetBool("parado", false);
-            GetComponent<Animator>().SetBool("saltar", true);
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0f);
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpPower), ForceMode2D.Impulse);
-            ensuelo = false;
-                        
-        }
-
-        timer2 += 2f *Time.deltaTime;
-        if (timer2 >= 0.1f)
-        {
-            GetComponent<Animator>().SetBool("saltar", false);
-            GetComponent<Animator>().SetBool("caer", false);
-            timer2 = 0;
-        }
-        
-
-        if (!ensuelo && !GetComponent<Animator>().GetBool("saltar"))
-        {
-            ensuelo = Physics2D.OverlapCircle(suelocheck.position, checkradius, sueloLayer);
-            GetComponent<Animator>().SetBool("caminando y salto", ensuelo);
-        }
-        
-        ensuelo = Physics2D.OverlapCircle(suelocheck.position, checkradius, sueloLayer);
-        GetComponent<Animator>().SetBool("parado", ensuelo);
-
-        if (Input.GetKey(KeyCode.RightArrow) && !GetComponent<Animator>().GetBool("agacharse") && !GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Patada"))
-        {
-            if (GetComponent<SpriteRenderer>().flipX == true)
-            {
-                GetComponent<SpriteRenderer>().flipX = false;
-            }
-            GetComponent<Animator>().SetBool("parado", false);
-            GetComponent<Animator>().SetBool("caminando", true);
-            GetComponent<Rigidbody2D>().velocity = new Vector2(velocidad, GetComponent<Rigidbody2D>().velocity.y);
-        }
-        else if (Input.GetKeyUp(KeyCode.RightArrow) && ensuelo && !GetComponent<Animator>().GetBool("patada"))
-        {
-            GetComponent<Animator>().SetBool("caminando", false);
-            GetComponent<Animator>().SetBool("parado", true);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) && !GetComponent<Animator>().GetBool("agacharse") && !GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Patada"))
-        {
-            if (GetComponent<SpriteRenderer>().flipX == false)
-            {
-                GetComponent<SpriteRenderer>().flipX = true;
-            }
-            GetComponent<Animator>().SetBool("parado", false);
-            GetComponent<Animator>().SetBool("caminando", true);
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-velocidad, GetComponent<Rigidbody2D>().velocity.y);
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftArrow) && ensuelo && !GetComponent<Animator>().GetBool("patada"))
-        {
-            GetComponent<Animator>().SetBool("caminando", false);
-            GetComponent<Animator>().SetBool("parado", true);
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            GetComponent<Animator>().SetBool("agacharse", true);
-        }
-        if (Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            GetComponent<Animator>().SetBool("agacharse", false);
-        }
-
-        if (Input.GetAxis("Fire1") > 0 && !disparo && ensuelo) {
-            timer -= Time.deltaTime;
-            if (timer <= 0) {
-                if (GetComponent<SpriteRenderer>().flipX == false)
-                {
-                    GetComponent<Rigidbody2D>().velocity = new Vector2(8f, GetComponent<Rigidbody2D>().velocity.y);
-                }
-                else {
-                    GetComponent<Rigidbody2D>().velocity = new Vector2(-8f, GetComponent<Rigidbody2D>().velocity.y);
-                }    
-                GetComponent<Animator>().SetBool("parado", true);
-                GetComponent<Animator>().SetBool("caminando", false);
-                GetComponent<Animator>().SetTrigger("patada");
-                disparo = true;
-                timer = 0.25f;
-            }
-            
-        }
-
-        else if (Input.GetAxisRaw("Fire1") < 1)
-        {
-            disparo = false;
-        }
-
-        if (!ensuelo) {
-
-            GetComponent<Animator>().SetBool("caer", true);
-
-        }
-        if (Input.GetKeyDown("c") && GetComponent<Animator>().GetBool("parado"))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 15f);
-            GetComponent<Animator>().SetTrigger("upper");
-
-        }
-
-        else if (Input.GetKeyDown("c") && GetComponent<Animator>().GetBool("caminando") && ensuelo)
-        {
-            GetComponent<Animator>().SetBool("upper caminando", true);
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 15f);
-            GetComponent<Animator>().SetTrigger("upper");
-
-        }
-
-  
- 
-
-        if (ensuelo) {
-            GetComponent<Animator>().SetBool("upper caminando", false);
-
-        }
-
-
-   
-
-
-    }
-
-   
-
-    
 }
