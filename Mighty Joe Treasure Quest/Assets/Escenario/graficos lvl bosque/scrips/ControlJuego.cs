2c7cf7e86236d53747e07426f2c73eb017b2abb1
CortesLeyton <38547297+CortesLeyton@users.noreply.github.com>
2018-04-30 22:49:06 -0300

    Movimiento del fondo
    movimiento parallax (sensacion de mapa infinito),  voy a testear si al colocar plataformas estas se quedas quietas o se mueven con el fondo

---
+++ Mighty Joe Treasure Quest/Assets/Escenario/graficos lvl bosque/scrips/ControlJuego.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlJuego : MonoBehaviour {
+
+    public float parallaxSpeed = 0.07f;
+    public float parallaxA = 0.07f;
+    public float parallaxB = 0.07f;
+    public float parallaxC = 0.01f;
+    public RawImage Fondo;
+    public RawImage Atras;
+    public RawImage Medio;
+    public RawImage Adelante;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float VelocidadFinal = parallaxSpeed * Time.deltaTime;
+        float VelocidadFinalA = parallaxA * Time.deltaTime;
+        float VelocidadFinalB = parallaxB * Time.deltaTime;
+        float VelocidadFinalC = parallaxC * Time.deltaTime;
+        Fondo.uvRect = new Rect(Fondo.uvRect.x + VelocidadFinal, 0f, 1f, 1f);
+        Atras.uvRect = new Rect(Atras.uvRect.x + VelocidadFinalA, 0f, 1f, 1f);
+        Medio.uvRect = new Rect(Medio.uvRect.x + VelocidadFinalB, 0f, 1f, 1f);
+        Adelante.uvRect = new Rect(Adelante.uvRect.x + VelocidadFinalC, 0f, 1f, 1f);
+    }
+}
