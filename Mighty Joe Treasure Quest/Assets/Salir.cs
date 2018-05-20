8cef265c9a5276e44eddd34311427480b4c2baa9
efecepanchito1 <38562151+efecepanchito1@users.noreply.github.com>
2018-05-19 21:13:41 -0400

    implementando boton salir(prueba 1)
    boton salir

---
+++ Mighty Joe Treasure Quest/Assets/Salir.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Salir : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Application.Quit();
+            Debug.Log("ya sali wey");
+
+        }
+    }
+
+}
