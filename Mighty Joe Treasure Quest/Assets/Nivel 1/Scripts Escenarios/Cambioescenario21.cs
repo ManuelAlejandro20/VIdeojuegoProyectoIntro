b5fda4537333d18feca5869792dbac1bc20f027a
gokubastiaan <goku_bastian@hotmail.com>
2018-05-20 19:18:21 -0400

    commit 20/05
    escenario 3 1er nivel

---
+++ Mighty Joe Treasure Quest/Assets/Nivel 1/Scripts Escenarios/Cambioescenario21.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Cambioescenario21 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerStay2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SceneManager.LoadScene("Escenario 3 1er nivel", LoadSceneMode.Single);
+        }
+    }
+}
