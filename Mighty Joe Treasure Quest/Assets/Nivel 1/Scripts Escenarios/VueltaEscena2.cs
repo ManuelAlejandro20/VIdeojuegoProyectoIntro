0b4a1741e8ca15b14f7adc72346fa1c0ce8a69b8
gokubastiaan <goku_bastian@hotmail.com>
2018-05-20 20:51:33 -0400

    20/05
    arreglos

--- Mighty Joe Treasure Quest/Assets/Nivel 1/Scripts Escenarios/VueltaEscena2.cs
+++ Mighty Joe Treasure Quest/Assets/Nivel 1/Scripts Escenarios/VueltaEscena2.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Vueltaescena1 : MonoBehaviour {
+
+
+public class VueltaEscena2 : MonoBehaviour {
     public Transform jugador;
 	// Use this for initialization
 	void Start () {
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	 
+       
+    }
+    // Update is called once per frame
+    void Update () {
 		
 	}
     public void OnTriggerStay2D(Collider2D other)
@@ -18,8 +21,7 @@ public class Vueltaescena1 : MonoBehaviour {
         
         if (other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.UpArrow))
         {
-            SceneManager.LoadScene("Escenario 1 1er nivel");
+            SceneManager.LoadScene("Escenario 2 1er nivel");
         }
-            
     }
 }
