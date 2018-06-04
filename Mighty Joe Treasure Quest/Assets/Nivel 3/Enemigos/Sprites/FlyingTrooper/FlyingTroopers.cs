8e0d6cb43f48524636f8fa97adaf246974969148
ManuelAlejandro20 <38479974+ManuelAlejandro20@users.noreply.github.com>
2018-06-04 04:37:38 -0400

    Cooommit

---
+++ Mighty Joe Treasure Quest/Assets/Nivel 3/Enemigos/Sprites/FlyingTrooper/FlyingTroopers.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingTroopers : MonoBehaviour {
+
+    public GameObject principio;
+    public GameObject final;
+    public float velocidad;
+    float tiempo; 
+
+    Rigidbody2D rigid;
+    Rigidbody2D rigidinicio;
+    Rigidbody2D rigidfinal;
+
+    SpriteRenderer sprite;
+
+    bool haciafinal;
+    bool haciainicio;
+
+    void Awake() {
+
+        rigid = GetComponent<Rigidbody2D>();
+        rigidinicio = principio.GetComponent<Rigidbody2D>();
+        rigidfinal = final.GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    
+
+	void Start () {
+
+        rigid.position = rigidinicio.position;
+        haciafinal = true;
+	}
+	
+	
+	void Update () {
+
+        if (haciafinal)
+        {
+            sprite.flipX = false;
+            rigid.position = Vector3.MoveTowards(rigid.position, rigidfinal.position, velocidad);
+            if (rigid.position == rigidfinal.position) {
+                tiempo += Time.deltaTime;
+                if (tiempo >= 5.5f)
+                {
+                    rigidinicio.position = new Vector2(rigidinicio.position.x, Random.Range(-50f, 50f));
+                    haciafinal = false;
+                    haciainicio = true;
+                    tiempo = 0;
+                }
+            }
+
+        }
+        else if(haciainicio) {
+
+            sprite.flipX = true;
+            rigid.position = Vector3.MoveTowards(rigid.position, rigidinicio.position, velocidad);
+            if (rigid.position == rigidinicio.position)
+            {
+                tiempo += Time.deltaTime;
+                if (tiempo >= 5.5f)
+                {
+                    rigidfinal.position = new Vector2(rigidfinal.position.x, Random.Range(-50f, 50f));
+                    haciafinal = true;
+                    haciainicio = false;
+                    tiempo = 0;
+                }
+            }
+        }
+       
+
+
+
+    }
+}
