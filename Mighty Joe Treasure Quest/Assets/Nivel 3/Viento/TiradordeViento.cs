9f0051f15405f7d46acba00230771bdf620c42f9
ManuelAlejandro20 <38479974+ManuelAlejandro20@users.noreply.github.com>
2018-06-04 08:16:25 -0400

    Commit

---
+++ Mighty Joe Treasure Quest/Assets/Nivel 3/Viento/TiradordeViento.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiradordeViento : MonoBehaviour {
+
+    public GameObject vientoprefab;
+    public GameObject arriba;
+    public GameObject abajo;
+
+    bool abajobool = true;
+    bool arribabool = false;
+
+    RectTransform recttran;
+    RectTransform recttranarriba;
+    RectTransform recttranabajo;
+
+    void Awake() {
+
+        recttran = GetComponent<RectTransform>();
+        recttranarriba = arriba.GetComponent<RectTransform>();
+        recttranabajo = abajo.GetComponent<RectTransform>();
+
+    }
+	void Start () {
+
+        recttran.position = recttranarriba.position;
+
+
+    }
+	
+	void Update () {
+
+        if (arribabool)
+        {
+
+            recttran.position = Vector3.MoveTowards(recttran.position, recttranarriba.position, 0.5f);
+            if (recttran.position == recttranarriba.position)
+            {
+                abajobool = true;
+                arribabool = false;
+
+            }
+        }
+
+        else if (abajobool)
+        {
+
+
+            recttran.position = Vector3.MoveTowards(recttran.position, recttranabajo.position, 0.5f);
+            if (recttran.position == recttranabajo.position)
+            {
+                abajobool = false;
+                arribabool = true;
+
+            }
+        }
+
+        
+
+
+    }
+
+    void funcionviento()
+    {
+        Instantiate(vientoprefab, transform.position, Quaternion.identity);
+    }
+}
