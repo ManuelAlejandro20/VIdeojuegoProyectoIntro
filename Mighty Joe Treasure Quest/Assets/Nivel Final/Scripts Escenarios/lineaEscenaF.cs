4aef74e9899030e41aa55e4beccac9f9b710556c
ManuelAlejandro20 <38479974+ManuelAlejandro20@users.noreply.github.com>
2018-06-06 00:00:30 -0400

    Commit

--- Mighty Joe Treasure Quest/Assets/Nivel Final/Scripts Escenarios/lineaEscenaF.cs
+++ Mighty Joe Treasure Quest/Assets/Nivel Final/Scripts Escenarios/lineaEscenaF.cs
@@ -7,24 +7,24 @@ public class lineaEscenaF : MonoBehaviour {
     public Transform desde;
     public Transform hasta;
 
-    // Use this for initialization
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
 
     }
     private void OnDrawGizmosSelected()
     {
-        if (desde != null && hasta != null) ;
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawLine(desde.position, hasta.position);
-        Gizmos.DrawSphere(desde.position, 1f);
-        Gizmos.DrawSphere(hasta.position, 1f);
+        if (desde != null && hasta != null) {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(desde.position, hasta.position);
+            Gizmos.DrawSphere(desde.position, 1f);
+            Gizmos.DrawSphere(hasta.position, 1f);
+        }
+        
 
 
     }
