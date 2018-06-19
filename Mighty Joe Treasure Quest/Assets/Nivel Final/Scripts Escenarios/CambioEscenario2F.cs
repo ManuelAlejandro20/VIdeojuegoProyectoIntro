6bc1e528014ae21feec2dd02e46f685fc31e86dc
efecepanchito1 <38562151+efecepanchito1@users.noreply.github.com>
2018-06-19 13:27:16 -0400

    commit

---
+++ Mighty Joe Treasure Quest/Assets/Nivel Final/Scripts Escenarios/CambioEscenario2F.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CambioEscenario2F : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnTriggerStay2D(Collider2D other)
+	{
+
+		if (other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			SceneManager.LoadScene("escena 2 nivel final", LoadSceneMode.Single);
+		}
+	}
+}
